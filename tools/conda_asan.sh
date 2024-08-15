1c0d63c54dc00ffe5e8d4a6a99d0378121edc7b2
Francesco Biscani <bluescarni@gmail.com>
2024-08-15 09:49:01 +0200

    Another fix.

--- tools/conda_asan.sh
+++ tools/conda_asan.sh
@@ -10,7 +10,7 @@ set -e
 sudo apt-get install wget
 
 # Install conda+deps.
-wget https://github.com/conda-forge/miniforge/releases/latest/download/${CONDA_INSTALLER_ARCH}.sh -O miniconda.sh
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-${CONDA_INSTALLER_ARCH}.sh -O miniconda.sh
 export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
