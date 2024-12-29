50b394923f1d19577879031cce1058d0a9ea8e2f
Francesco Biscani <bluescarni@gmail.com>
2024-12-29 18:04:03 +0100

    Debug.

--- tools/conda.sh
+++ tools/conda.sh
@@ -11,19 +11,10 @@ wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge
 export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
-conda create -y -p $deps_dir c-compiler cxx-compiler cmake ninja \
-    tbb-devel tbb libboost-devel heyoka fmt spdlog 'python=3.12' numpy \
-    pybind11 pandas astropy heyoka.py requests polars
+conda create -y -p $deps_dir c-compiler cxx-compiler cmake ninja
+# tbb-devel tbb libboost-devel heyoka fmt spdlog 'python=3.12' numpy \
+# pybind11 pandas astropy heyoka.py requests polars
 source activate $deps_dir
-
-# NOTE: not sure what is going on with conda OSX, but somehow
-# at this time installing c/cxx-compiler fetches a version
-# of clang(xx) earlier than the one used to produce the binary
-# packages.
-if [[ "${CONDA_INSTALLER_ARCH}" == "MacOSX"* ]]; then
-    conda remove -y c-compiler cxx-compiler
-    conda install -y 'clang=18.*' 'clangxx=18.*'
-fi
 
 # Workaround: install sgp4 and skyfield with pip
 # because the conda package for sgp4 on aarch64
