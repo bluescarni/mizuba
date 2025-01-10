2edd9e771da3c2f2603eacf565d29e455c8dd8fb
Francesco Biscani <bluescarni@gmail.com>
2025-01-10 15:14:58 +0100

    Allow to provide the gpes as a numpy array.

--- mizuba/_sgp4_polyjectory.py
+++ mizuba/_sgp4_polyjectory.py
@@ -15,47 +15,53 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from .core import polyjectory
+from .core import polyjectory, gpe_dtype
 import polars as pl
+from typing import Union
+import numpy as np
+
+# The fields expected to be in a dataframe
+# containing GPEs.
+_gpe_fields = {
+    "norad_id": pl.UInt64,
+    "epoch_jd1": float,
+    "epoch_jd2": float,
+    "n0": float,
+    "e0": float,
+    "i0": float,
+    "node0": float,
+    "omega0": float,
+    "m0": float,
+    "bstar": float,
+}
 
 
 def make_sgp4_polyjectory(
-    gpes: pl.DataFrame, jd_begin: float, jd_end: float
+    gpes: Union[pl.DataFrame, np.ndarray[gpe_dtype]], jd_begin: float, jd_end: float
 ) -> polyjectory:
     from .core import _make_sgp4_polyjectory
     import polars as pl
     import numpy as np
 
-    # TODO allow gpes directly as a numpy array of gpe.
+    if isinstance(gpes, pl.DataFrame):
+        # Make sure the expected fields in gpes have the correct type.
+        gpes = gpes.with_columns(
+            [pl.col(name).cast(_gpe_fields[name]) for name in _gpe_fields]
+        )
 
-    # Fields (and their types) to extract from gpes.
-    fields = {
-        "norad_id": pl.UInt64,
-        "epoch_jd1": float,
-        "epoch_jd2": float,
-        "n0": float,
-        "e0": float,
-        "i0": float,
-        "node0": float,
-        "omega0": float,
-        "m0": float,
-        "bstar": float,
-    }
+        # Extract the fields in a separate dataframe.
+        gpes = gpes.select([pl.col(name) for name in _gpe_fields])
 
-    # Make sure the expected fields in gpes have the correct type.
-    gpes = gpes.with_columns([pl.col(name).cast(fields[name]) for name in fields])
-
-    # Extract the fields in a separate dataframe.
-    gpes = gpes.select([pl.col(name) for name in fields])
-
-    # Convert to numpy.
-    # NOTE: since we enforced types and ordering of fields,
-    # it should not be necessary to cast manually to the gpe
-    # dtype.
-    gpes_arr = np.ascontiguousarray(gpes.to_numpy(structured=True))
+        # Convert to numpy.
+        # NOTE: since we enforced types and ordering of fields,
+        # it should not be necessary to cast manually to the gpe
+        # dtype.
+        gpes_arr = np.ascontiguousarray(gpes.to_numpy(structured=True))
+    else:
+        gpes_arr = gpes
 
     # Invoke the C++ function.
     return _make_sgp4_polyjectory(gpes_arr, jd_begin, jd_end)
 
 
-del polyjectory, pl
+del polyjectory, pl, gpe_dtype, Union, np
