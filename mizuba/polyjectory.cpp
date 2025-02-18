ca22d135e984d1f2fd137be70d4797cfad5d9539
Francesco Biscani <bluescarni@gmail.com>
2025-02-18 09:05:25 +0100

    Coverage fixes.

--- mizuba/polyjectory.cpp
+++ mizuba/polyjectory.cpp
@@ -704,15 +704,19 @@ polyjectory::polyjectory(const std::filesystem::path &orig_traj_file_path,
                 // Check the file sizes.
                 if (boost::filesystem::file_size(traj_path) != tot_num_traj_values * sizeof(double)) [[unlikely]] {
                     throw std::invalid_argument(
+                        // LCOV_EXCL_START
                         fmt::format("Invalid trajectory data file passed to the constructor of a polyjectory: the "
                                     "expected size in bytes is {} but the actual size is {} instead",
+                                    // LCOV_EXCL_STOP
                                     static_cast<std::size_t>(tot_num_traj_values * sizeof(double)),
                                     boost::filesystem::file_size(traj_path)));
                 }
                 if (boost::filesystem::file_size(time_path) != tot_num_time_values * sizeof(double)) [[unlikely]] {
                     throw std::invalid_argument(
+                        // LCOV_EXCL_START
                         fmt::format("Invalid time data file passed to the constructor of a polyjectory: the "
                                     "expected size in bytes is {} but the actual size is {} instead",
+                                    // LCOV_EXCL_STOP
                                     static_cast<std::size_t>(tot_num_time_values * sizeof(double)),
                                     boost::filesystem::file_size(time_path)));
                 }
