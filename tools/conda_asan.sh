5c5d05a072643254a1d7bde2bcb31a0002414487
Francesco Biscani <bluescarni@gmail.com>
2025-05-09 12:45:12 +0200

    Tentative CI workaround.

--- tools/conda_asan.sh
+++ tools/conda_asan.sh
@@ -14,9 +14,11 @@ wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge
 export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
+# NOTE: the zstandard pin here is due to this issue:
+# https://github.com/conda-forge/zstandard-feedstock/pull/61#issuecomment-2841916192
 conda create -y -p $deps_dir c-compiler cxx-compiler cmake ninja \
     tbb-devel tbb libboost-devel heyoka fmt spdlog 'python=3.12' numpy \
-    pybind11 astropy heyoka.py sgp4 requests polars
+    pybind11 astropy heyoka.py sgp4 requests polars 'zstandard=0.23.0=py312hef9b889_1'
 source activate $deps_dir
 
 # Create the build dir and cd into it.
